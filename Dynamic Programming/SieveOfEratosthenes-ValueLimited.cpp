142feae9b28c3da7a3a0c09a75e0ac9873d50d93
Ajit Panigrahi <ajitzero@gmail.com>
2017-12-12 21:56:04 +0530

    Added SieveOfEratosthenes-ValueLimited.cpp
    First commit. Contains basic explanations and relevant sources.

---
+++ Dynamic Programming/SieveOfEratosthenes-ValueLimited.cpp
@@ -0,0 +1,74 @@
+/**
+ * 
+ * Title: Sieve Of Eratosthenes [Value limited]
+ * Description: Display all primes numbers upto a given value N as a limiter.
+ * Author: Ajit Panigrahi
+ * GitHub: https://github.com/AjitZero
+ * 
+ */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+/**
+ * Creates and displays all prime numbers less than or equal to N
+ * @param N: Upper bound range limiter
+ */
+void SieveOfEratosthenes(int N) {
+    
+    /**
+     * Creating a boolean array for memoization. The index is the number tested for primality.
+     */
+    bool store[N + 1];
+
+    /**
+     * Initializes all elements of the array `store` as `true`.
+     *
+     * Header file required: `cstring`
+     * Source: http://en.cppreference.com/w/cpp/string/byte/memset
+     */
+    memset(store, true, sizeof(store));
+
+    /**
+     * Looping through all elements from 2 till N, using the limit as square root of N
+     *
+     * Source: https://stackoverflow.com/q/5811151/808018
+     */
+    for (int i = 2; i * i <= N; ++i) {
+
+        /**
+         * If element is assigned `false` then we have already calculated that it is not prime
+         */
+        if (store[i]) {
+
+            /**
+             * At this point `store[i]` has a truth value of `true`.
+             * We need to mark each of its multiples as not prime by assigning a truth value of `false` to them.
+             */
+            for (int j = i * 2; j <= N; j += i) {
+                store[j] = false;
+            }
+        }
+    }
+
+    /**
+     * Display all indices which store a truth value of `true`
+     */
+    for (int i = 2; i <= N; ++i) {
+        if (store[i]) {
+            cout << i << " ";
+        }
+    }
+}
+
+/**
+ * Driver function
+ */
+int main() {
+    int limit;
+    cin >> limit;
+    SieveOfEratosthenes(limit);
+    return 0;
+}
