d38d62b5dfaa3138d56149218973464b04fa0849
AjitZero <ajitzero@gmail.com>
2018-07-23 23:21:40 +0530

    Formatted 0-1 Knapsack Problem

--- Dynamic Programming/0-1KnapsackProblem.cpp
+++ Dynamic Programming/0-1KnapsackProblem.cpp
@@ -12,10 +12,8 @@
 #include <vector>
 using namespace std;
 
-typedef long long ll;
-
 /**
- * Calculate the maximum value that can be accumulated within a given weight limit.
+ * Calculates the maximum value that can be accumulated within a given weight limit.
  * 
  * @param  values       [List of values]
  * @param  weights      [List of weights]
@@ -23,14 +21,14 @@ typedef long long ll;
  * @param  maxCapacity  [Weight limit]
  * @return              [Maximum accumulated value]
  */
-ll KnapsackProblem(vector<ll> const &values, vector<ll> const &weights, const ll noOfElements, const ll maxCapacity) {
+int KnapsackProblem(vector<int> const &values, vector<int> const &weights, const int noOfElements, const int maxCapacity) {
 
 	// `memo` is a two-dimensional vector used for memoization, with `noOfElements + 1` rows and `maxCapacity + 1` columns
-	ll memo[noOfElements + 1][maxCapacity + 1];
+	int memo[noOfElements + 1][maxCapacity + 1];
 
 	// Bottom-Down Approach
-	for (ll i = 0; i <= noOfElements; ++i) {
-		for (ll j = 0; j <= maxCapacity; ++j) {
+	for (int i = 0; i <= noOfElements; ++i) {
+		for (int j = 0; j <= maxCapacity; ++j) {
 
 			// Initializing first row and column to `0`
 			if (i == 0 || j == 0) {
@@ -57,19 +55,19 @@ ll KnapsackProblem(vector<ll> const &values, vector<ll> const &weights, const ll
 
 // Driver function
 int main() {
-	ll noOfElements;
+	int noOfElements;
 	cin >> noOfElements;
-	vector<ll> values(noOfElements), weights(noOfElements);
+	vector<int> values(noOfElements), weights(noOfElements);
 
 	// Enter `noOfElements` values:
-	for (ll i = 0; i < noOfElements; ++i)
+	for (int i = 0; i < noOfElements; ++i)
 		cin >> values[i];
 
 	// Enter `noOfElements` weights:
-	for (ll i = 0; i < noOfElements; ++i)
+	for (int i = 0; i < noOfElements; ++i)
 		cin >> weights[i];
 
-	ll maxCapacity;
+	int maxCapacity;
 	cin >> maxCapacity;
 
 	cout << KnapsackProblem(values, weights, noOfElements, maxCapacity) << endl;
