f16f7c072b0ed9e3cd6634f46bdcb57c983f16d9
Ajit Panigrahi <ajitzero@gmail.com>
2020-10-02 16:49:28 +0530

    Reformatted the code

--- Big Integers/BigSorting.cpp
+++ Big Integers/BigSorting.cpp
@@ -1,52 +1,76 @@
-/*
+/**
+*
 * Title: Big Sorting
 * Description: Given a list of numbers, including Big Integers of an arbitrary number of digits, sort it in ascending order.
 * Author: Vanamala Srikanth
 * GitHub: https://github.com/srikanth7785
 * Twitter: https://twitter.com/srikanth7785
+*
 */
 
-#include<iostream>
-#include<vector>
-#include<algorithm> //for sort()
+#include <iostream>
+#include <vector>
+#include <algorithm> // for sort()
+
 using namespace std;
 
-bool customSorting(string a, string b){
-    //below, first two conditions, will check if one of the strings are having a `-ve`..
-    // if only first string is `-ve`
-    if(a[0] == '-' and b[0] != '-')
+bool customComparator(string a, string b) {
+    // Handling negative numbers:
+    // If only first string is `-ve`
+    if (a[0] == '-' && b[0] != '-') {
         return true;
-    // if only second string is `-ve`
-    if(a[0] != '-' and b[0] == '-')
+    }
+
+    // If only second string is `-ve`
+    if (a[0] != '-' && b[0] == '-') {
         return false;
-    //if both are negative
-    if(a[0] == '-' and b[0] == '-'){
-        if(a.length() == b.length())
+    }
+
+    // If both are negative
+    if (a[0] == '-' && b[0] == '-') {
+        if (a.length() == b.length()) {
+            // If both the strings are of same length
             return a > b;
+        }
         return a.length() > b.length();
     }
-    // below conditions will check only when both are `+ve`
-    if(a.length() == b.length()) // if both the strings are of same length
-        return a < b; // check if first string is less than the second string
-    return a.length() < b.length(); // if both strings are of different length, then the one with smallest length will be the smallest number
+
+    // Handling positive numbers:
+    if (a.length() == b.length()) {
+        // If both the strings are of same length
+        return a < b;
+    }
+    // If both strings are of different length,
+    // then the one with smaller length will be the smallest number
+    return a.length() < b.length();
 }
 
-int main(){
-    int n;
-    cin >> n;
-    vector<string> numbers;//an array can also be used instead
-    
-    for(int i = 0;i < n; i++)
-    {string temp; cin >> temp; numbers.push_back(temp);}
-    
-    sort(numbers.begin(),numbers.end(),customSorting); // this is an inbuilt method available in `algorithm`. it will call `customSorting()` for each comparision
-    
-    for(string num : numbers) //finally, print the output, which is sorted.
-    cout << num << endl;
+// Driver function
+int main() {
+    int noOfNumbers;
+    cin >> noOfNumbers;
+    vector<string> BigIntegers;
+
+    for (int i = 0; i < noOfNumbers; ++i) {
+        string currNumber;
+        cin >> currNumber;
+        BigIntegers.push_back(currNumber);
+    }
+
+    // Header file required: `algorithm`.
+    // It passes `customComparator()` for each comparision
+    sort(BigIntegers.begin(), BigIntegers.end(), customComparator);
+
+    for (string number : BigIntegers) {
+        cout << number << endl;
+    }
+    return 0;
 }
 /*
--------------------------
+
 Sample Input:
+--------------------
+
 26
 -29587466846
 1
@@ -75,7 +99,8 @@ Sample Input:
 987456215
 -8461844655468625
 
-Sample Output: 
+Sample Output:
+--------------------
 
 -999999999999999999999999999
 -8461844655468628
